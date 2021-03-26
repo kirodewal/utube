ce2668a9f9f471573235cef8ede0d9a5016ebbe0
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-03-26 13:19:10 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,22 +1,24 @@
+# © @Kirodewal
+
 import os
 
 class Config:
 
-    BOT_TOKEN = os.environ.get("1691360925:AAHD9-q-uwhJDUutbOucGCGEEQ3IoyccGok")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
     
     SESSION_NAME = os.environ.get("SESSION_NAME", 'youtubeitbot')
 
-    API_ID = int(os.environ.get("1144902"))
+    API_ID = int(os.environ.get("API_ID"))
 
-    API_HASH = os.environ.get("e743e5a4f35076e4c558a4bd713082e9")
+    API_HASH = os.environ.get("API_HASH")
 
-    CLIENT_ID = os.environ.get("285126996267-d58o2euqvb47sbar0bmsid5fmpmgt8b4.apps.googleusercontent.com")
+    CLIENT_ID = os.environ.get("CLIENT_ID")
 
-    CLIENT_SECRET = os.environ.get("Kbmpa7_0vM317_jV8KtGJ1CT")
+    CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 
-    BOT_OWNER = int(os.environ.get("754495556"))
+    BOT_OWNER = int(os.environ.get("BOT_OWNER"))
     
-    AUTH_USERS_TEXT = os.environ.get("754495556", '')
+    AUTH_USERS_TEXT = os.environ.get("AUTH_USERS", '')
 
     AUTH_USERS = [BOT_OWNER, 374321319] + ([int(user.strip()) for user in AUTH_USERS_TEXT.split(",")] if AUTH_USERS_TEXT else [])
     
@@ -38,6 +40,3 @@ class Config:
             UPLOAD_MODE = False
 
     CRED_FILE = "auth_token.txt"
-
-
-
